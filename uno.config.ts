78c972eac1b321c703be614e18fd3950c65f6922
mylu0x <mylu0x@gmail.com>
2025-11-08 13:58:26 +0900

    chore(cfg): setup UnoCSS config

---
+++ uno.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, presetIcons, presetWind3, transformerDirectives, transformerVariantGroup } from 'unocss';
+
+export default defineConfig({
+  presets: [
+    presetWind3(),
+    presetIcons()
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup()
+  ]
+});
