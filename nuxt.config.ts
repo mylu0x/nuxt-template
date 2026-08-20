3bca7bd9b509fabd399f428557607e29249c6a31
Mylu <229919353+mylu0x@users.noreply.github.com>
2026-08-20 15:32:39 +0900

    chore(deps): add @nuxt/fonts and /icons modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['@nuxt/fonts', 'nuxt-icons'],
   app: {
     head: {
       title: 'Your Title Here',
